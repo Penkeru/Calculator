8d84c97a10fd3b60b4e923abb5a0c83597dee1ee
Uriel Penker <penkeru@gmail.com>
2016-07-09 17:19:32 +0300

    - removed some unnesscary code. - modulized the calculator.

--- js/main.js
+++ js/main.js
@@ -18,19 +18,19 @@ var StandardCalc =  (function () {
   };
 
   return{
-    plus:plus,
-    minus:minus,
-    multiply:multiply,
-    divide:divide
+    '+':plus,
+    '-':minus,
+    '*':multiply,
+    '/':divide
   }
 })();
 
 var ScientificCalc = (function (Module) {
-    Module.power = function (x , y) {
+    Module['^'] = function (x , y) {
         return Math.pow(parseFloat(x),parseFloat(y));
     };
 
-    Module.root = function (x , y) {
+    Module['root'] = function (x , y) {
       if(x!==0){
         return Math.pow(parseFloat(y),1/parseFloat(x));
       }
@@ -41,7 +41,7 @@ var ScientificCalc = (function (Module) {
 })(StandardCalc || {});
 
 var ProgrammerCalc = (function (Module) {
-    Module.mod = function (x , y) {
+    Module['%'] = function (x , y) {
         return parseFloat(x)%parseFloat(y);
     };
 
@@ -50,93 +50,105 @@ var ProgrammerCalc = (function (Module) {
 })(StandardCalc || {});
 
 
-var CalculatorMAP = {
-      'standard':StandardCalc,
-      'scientific':ScientificCalc,
-      'programmer':ProgrammerCalc
-};
-var OperatorTextMap = {
-      '+':'plus',
-      '-':'minus',
-      '/': 'divide',
-      '*':'multiply',
-      '^':'power',
-      'root':'root',
-      '%':'mod'
-}
 
-var selectedCalculator = StandardCalc;
-var selectboxElement = document.getElementById("operatorSelect");
-var resultSpan = document.getElementById("calcResult");
-var calcOptions = document.getElementsByName("calcOption");
-var operand1 = document.getElementById("firstOperand");
-var operand2 = document.getElementById("secondOperand");
-for(calcOption in calcOptions) {
-    calcOptions[calcOption].onclick = function() {
-        selectedCalculator = CalculatorMAP[this.value];
-        clearInput();
-        removeOptions();
-        addOptions(this.value);
+
+var Calculator = function(calcType){
+    var CalculatorMAP = {
+        'standard':StandardCalc,
+        'scientific':ScientificCalc,
+        'programmer':ProgrammerCalc
+    };
+    var selectedCalculator = CalculatorMAP[calcType] || StandardCalc;
+    var selectboxElement = document.getElementById("operatorSelect");
+    var resultSpan = document.getElementById("calcResult");
+
+    var operand1 = document.getElementById("firstOperand");
+    var operand2 = document.getElementById("secondOperand");
+
+    var calcOptions = document.getElementsByName("calcOption");
+
+    var clearInput = function(){
+        operand1.value ="";
+        operand2.value ="";
+        resultSpan.innerHTML = "";
+    };
+
+    var removeOptions = function(){
+        var i;
+        for(i=selectboxElement.options.length-1;i>=4;i--)
+        {
+            selectboxElement.remove(i);
+        }
+    };
+
+    var addOptions = function(calcType){
+        if(calcType === 'scientific'){
+            var opt = document.createElement("option");
+            opt.value = '^';
+            opt.text = '^';
+            selectboxElement.add(opt);
+            var opt = document.createElement("option");
+            opt.value = 'root';
+            opt.text = 'root';
+            selectboxElement.add(opt);
+        }
+        else if(calcType ==='programmer'){
+            var opt = document.createElement("option");
+            opt.value = '%';
+            opt.text = '%';
+            selectboxElement.add(opt);
+        }
+    };
+
+
+
+    var calculate = function(){
+        if(operand1.value && operand2.value && selectboxElement.value){
+            var result = selectedCalculator[selectboxElement.value](operand1.value,operand2.value);
+            resultSpan.innerHTML = Math.round(result * 10000) / 10000;
+        }
+    };
+
+
+
+    var init = function(){
+        for(calcOption in calcOptions) {
+            calcOptions[calcOption].onclick = function() {
+                selectedCalculator = CalculatorMAP[this.value];
+                clearInput();
+                removeOptions();
+                addOptions(this.value);
+            }
+        }
+        selectboxElement.addEventListener("change", calculate);
+        operand1.addEventListener("input", calculate);
+        operand2.addEventListener("input", calculate);
+    };
+
+    return {
+        init:init
     }
-}
 
 
 
-
-function calculate(){
-  if(operand1.value && operand2.value && selectboxElement.value){
-      var result = selectedCalculator[selectboxElement.value](operand1.value,operand2.value);
-      resultSpan.innerHTML = Math.round(result * 10000) / 10000;
-  }
-}
-
-
-function clearInput(){
-  operand1.value ="";
-  operand2.value ="";
-  resultSpan.innerHTML = "";
 };
 
-function removeOptions()
-{
-    var i;
-    for(i=selectboxElement.options.length-1;i>=4;i--)
-    {
-        selectboxElement.remove(i);
-    }
-}
+var isNumberKey = function(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode != 46 && charCode > 31
+        && (charCode < 48 || charCode > 57))
+        return false;
+
+    return true;
+};
 
 
-function addOptions(calcType){
-  if(calcType === 'scientific'){
-      var opt = document.createElement("option");
-      opt.value = 'power';
-      opt.text = '^';
-      selectboxElement.add(opt);
-      var opt = document.createElement("option");
-      opt.value = 'root';
-      opt.text = 'root';
-      selectboxElement.add(opt);
-  }
-  else if(calcType ==='programmer'){
-      var opt = document.createElement("option");
-      opt.value = 'mod';
-      opt.text = '%';
-      selectboxElement.add(opt);
-  }
-}
+window.onload = function() {
+    var calculator = new Calculator();
+    calculator.init();
+};
 
 
 
-function isNumberKey(evt){
-          var charCode = (evt.which) ? evt.which : evt.keyCode;
-          if (charCode != 46 && charCode > 31
-            && (charCode < 48 || charCode > 57))
-             return false;
 
-          return true;
-}
 
-selectboxElement.addEventListener("change", calculate);
-operand1.addEventListener("input", calculate);
-operand2.addEventListener("input", calculate);
